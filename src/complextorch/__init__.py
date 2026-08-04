e294e393feaca5bde4beff729801eec7151ec6e8
Rubén Herzog <rubenherzog@users.noreply.github.com>
2026-08-04 09:05:17 +0200

    Expose shared SSDI projection APIs and bump version

--- src/complextorch/__init__.py
+++ src/complextorch/__init__.py
@@ -4,13 +4,15 @@ from .representations import LinearDynamicalSystem, VARSystem, build_var_system,
 from .selection import EpochTimeSeriesSplit, VAROrderSearchCV, VAROrderSearchResult
 from .simulate import demo_var, random_stable_var, simulate_var
 from .var import VAR, VARParameters
-from .control import solve_dare, innovations_form, reduce_state_space, dynamical_dependence, stochastic_interaction
+from .control import solve_dare, innovations_form, reduce_state_space, project_state_space, dynamical_dependence, stochastic_interaction, optimise_dynamical_dependence_projection, ProjectionSearchResult
 from .state_space import kalman_filter, kalman_smoother, N4SID, LinearGaussianEM
 
-__all__=[
-"VAR","VARParameters","VARSystem","LinearDynamicalSystem","build_var_system","companion_matrix",
-"EpochTimeSeriesSplit","VAROrderSearchCV","VAROrderSearchResult","simulate_var","random_stable_var","demo_var",
-"from_complexbox_timeseries","to_complexbox_timeseries","from_complexbox_var","to_complexbox_var",
-"solve_dare","innovations_form","reduce_state_space","dynamical_dependence","stochastic_interaction",
-"kalman_filter","kalman_smoother","N4SID","LinearGaussianEM"]
-__version__="0.2.0"
+__all__ = [
+    "VAR", "VARParameters", "VARSystem", "LinearDynamicalSystem", "build_var_system", "companion_matrix",
+    "EpochTimeSeriesSplit", "VAROrderSearchCV", "VAROrderSearchResult", "simulate_var", "random_stable_var", "demo_var",
+    "from_complexbox_timeseries", "to_complexbox_timeseries", "from_complexbox_var", "to_complexbox_var",
+    "solve_dare", "innovations_form", "reduce_state_space", "project_state_space", "dynamical_dependence",
+    "stochastic_interaction", "optimise_dynamical_dependence_projection", "ProjectionSearchResult",
+    "kalman_filter", "kalman_smoother", "N4SID", "LinearGaussianEM",
+]
+__version__ = "0.3.0"
