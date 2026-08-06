6e1dda5ccff55b8a318b8e303e6a7dd97caf62c4
Rubén Herzog <rubenherzog@users.noreply.github.com>
2026-08-06 15:37:10 +0200

    Export state-space order search API

--- src/complextorch/__init__.py
+++ src/complextorch/__init__.py
@@ -2,6 +2,7 @@
 from .adapters import from_complexbox_timeseries, from_complexbox_var, to_complexbox_timeseries, to_complexbox_var
 from .representations import StateSpaceModel, VARSystem, build_var_system, companion_matrix
 from .selection import EpochTimeSeriesSplit, VAROrderSearchCV, VAROrderSearchResult, VAROrderSelectionIC, VARInformationCriteriaResult, StateSpaceOrderSelection, StateSpaceOrderSelectionResult
+from .state_space_selection import StateSpaceOrderSearchCV, StateSpaceOrderSearchResult, StateSpaceOrderScore
 from .simulate import demo_var, random_stable_var, simulate_var, automatic_burnin, random_correlation_matrix, random_positive_definite_covariance
 from .var import VAR, VARParameters
 from .control import solve_dare, solve_generalized_dare, innovations_form, InnovationsStateSpace, var_to_innovations_state_space, reduce_innovations_state_space, innovations_transfer_function, reduce_state_space, project_state_space, dynamical_dependence, stochastic_interaction, optimise_dynamical_dependence_projection, ProjectionSearchResult
@@ -12,12 +13,12 @@ from .measures.secondary import WhitenessResult, consistency, residual_whiteness
 __all__ = [
     "VAR", "VARParameters", "VARSystem", "StateSpaceModel", "build_var_system", "companion_matrix",
     "EpochTimeSeriesSplit", "VAROrderSearchCV", "VAROrderSearchResult", "VAROrderSelectionIC", "VARInformationCriteriaResult",
-    "StateSpaceOrderSelection", "StateSpaceOrderSelectionResult",
+    "StateSpaceOrderSelection", "StateSpaceOrderSelectionResult", "StateSpaceOrderSearchCV", "StateSpaceOrderSearchResult", "StateSpaceOrderScore",
     "simulate_var", "automatic_burnin", "random_stable_var", "random_correlation_matrix", "random_positive_definite_covariance", "demo_var",
     "from_complexbox_timeseries", "to_complexbox_timeseries", "from_complexbox_var", "to_complexbox_var",
     "solve_dare", "solve_generalized_dare", "innovations_form", "InnovationsStateSpace", "var_to_innovations_state_space", "reduce_innovations_state_space", "innovations_transfer_function", "reduce_state_space", "project_state_space", "dynamical_dependence", "stochastic_interaction", "optimise_dynamical_dependence_projection", "ProjectionSearchResult",
     "kalman_filter", "kalman_smoother", "N4SID", "LarimoreStateSpace", "LinearGaussianEM",
-    "ModelMeasureConfig", "ModelMeasureContext", "build_measure_context", "compute_all_model_measures", "model_autocovariances", "phiid_from_model", "temporal_mvgc", "spectral_mvgc",
+    "ModelMeasureConfig", "ModelMeasureContext", "build_measure_context", "compute_all_model_measures", "model_autocovariances", "phiid_from_model", "spectral_mvgc", "temporal_mvgc",
     "WhitenessResult", "consistency", "residual_whiteness", "mvgc_pvalue", "significance",
 ]
 __version__ = "0.8.0"
