719118fcb190efde2edf56eb12c13abd26e5d634
Rubén Herzog <rubenherzog@users.noreply.github.com>
2026-08-07 13:30:13 +0200

    Expose unified DD optimizer dispatcher

--- src/complextorch/__init__.py
+++ src/complextorch/__init__.py
@@ -21,6 +21,12 @@ from .control import (
     stochastic_interaction,
     var_to_innovations_state_space,
 )
+from .dd import (
+    DDObjective,
+    DDOptimizationResult,
+    DDOptimizer,
+    optimise_dynamical_dependence,
+)
 from .dd_optimization import (
     DDGradientSearchResult,
     innovations_proxy_sequence,
@@ -94,6 +100,9 @@ from .var import VAR, VARParameters
 
 __all__ = [
     "DDGradientSearchResult",
+    "DDObjective",
+    "DDOptimizationResult",
+    "DDOptimizer",
     "DDRiemannianSearchResult",
     "N4SID",
     "VAR",
@@ -135,6 +144,7 @@ __all__ = [
     "kalman_smoother",
     "model_autocovariances",
     "mvgc_pvalue",
+    "optimise_dynamical_dependence",
     "optimise_dynamical_dependence_projection",
     "optimise_dynamical_dependence_proxy",
     "optimise_dynamical_dependence_proxy_riemannian",
