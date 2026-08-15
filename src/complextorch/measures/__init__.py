f9f317f83a247c52c8ab0ef294b971e11f9bca2a
Rubén Herzog <rubenherzog@users.noreply.github.com>
2026-08-16 00:39:30 +0200

    Expose full-past emergence helper

--- src/complextorch/measures/__init__.py
+++ src/complextorch/measures/__init__.py
@@ -34,7 +34,7 @@ from .secondary import (
 )
 from .gaussian import gaussian_entropy, conditional_covariance, gaussian_mutual_information, gaussian_conditional_mutual_information, total_correlation, dual_total_correlation, o_information, s_information, local_gaussian_mutual_information
 from .dynamics import autocovariances, entropy_rate, predictive_information, active_information_storage, transfer_function, inverse_transfer_function, cross_spectral_density, spectral_entropy
-from .emergence import emergence_from_autocovariances, emergence_from_model, emergence_measures, emergence_from_observations
+from .emergence import emergence_from_autocovariances, emergence_from_full_past, emergence_from_model, emergence_measures, emergence_from_observations
 from .cmem import CMemResult, cmem1_curve, cmem1_total, cmem3_curve, cmem3_lag_decomposition, cmem3_total, compute_cmem
 from .criticality import covariance_amplification, dominant_timescale, stability_margin
 from .mvgc import state_space_temporal_mvgc, state_space_spectral_mvgc, integrate_spectral_mvgc, pairwise_spectral_gc
